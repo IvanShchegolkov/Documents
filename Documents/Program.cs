fb10637f7b47782cf52e542ae7cec95f4b16be78
IvanShchegolkov <sheg.ivan@gmail.com>
2022-02-15 09:51:55 +0300

    Добавьте файлы проекта.

---
+++ Documents/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Reflection;
+using Documents;
+
+namespace Documents
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    string path = Path.GetFullPath(@"..\..\..\");
+        //    Console.WriteLine(path)
+        //}
+        static async Task Main()
+        {
+            string path = Path.GetFullPath(@"..\..\..\" + @$"\test.json");
+            StagesList stages = await JsonFileReader.ReadAsync<StagesList>(path);
+
+
+
+
+            Console.WriteLine(path);
+            Console.WriteLine(stages);
+        }
+        public static class JsonFileReader
+        {
+            public static async Task<T> ReadAsync<T>(string filePath)
+            {
+                using FileStream stream = File.OpenRead(filePath);
+                return await JsonSerializer.DeserializeAsync<T>(stream);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+//{
+//    "Stages": [
+//      {
+//        "Name": "Согласование инициатором",
+//    "Performer": "Инициатор"
+//      },
+//    {
+//        "Name": "Согласование инициатором",
+//    "Performer": "Инициатор"
+//    }
+//  ]
+//}
---
+++ Documents/Stages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Documents
+{
+    internal class Stages
+    {
+        public string Name { get; set; }
+        public string Performer { get; set; }
+    }
+    internal class StagesList
+    {
+        public List<Stages> Stages { get; set; }
+    }
+}
